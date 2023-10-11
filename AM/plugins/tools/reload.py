fca0e9572aa7ef97c1090e66068343ab5b447699
AMBOT <144463605+AMBOTTEST@users.noreply.github.com>
2023-10-12 00:46:28 +0530

    Update reload.py

--- AM/plugins/tools/reload.py
+++ AM/plugins/tools/reload.py
@@ -92,7 +92,7 @@ async def close_menu(_, CallbackQuery):
         await CallbackQuery.answer()
         await CallbackQuery.message.delete()
         await CallbackQuery.message.reply_text(
-            f"𝓒𝓵𝓸𝓼𝓮𝓭 𝓑𝔂 ➣ {CallbackQuery.from_user.mention}"
+            f"𝓒𝓵𝓸𝓼𝓮𝓭 𝓑𝔂 ➣ {CallbackQuery.from_user.mention}\n{CallbackQuery.from_user.mention} ➣ @Logs_Gban"
         )
     except:
         pass
